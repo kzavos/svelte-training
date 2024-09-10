9fd2e1a810f4d7555436bb1f5d5e6766da5c3e84
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-10 12:35:08 +0300

    Add the first blog posts content

--- src/routes/projects/project-1/+page.svelte
+++ src/routes/projects/project-1/+page.svelte
@@ -1,35 +1,31 @@
 <script>
-	export let title;
-	export let date;
-	export let author;
-	export let content;
 </script>
 
-<article class="blog-post">
-	<h1 class="blog-post-title">Welcome friend!</h1>
-	<p class="blog-post-meta">
+<article class="container mx-auto max-w-3xl break-words px-4 py-8">
+	<!-- <div class="prose max-w-none">
+		<img
+			src="/images/project-images/welcome.jpg"
+			alt="Welcome"
+			class="mb-6 w-full rounded-lg shadow-md"
+		/>
+	</div> -->
+	<h1 class="mb-4 text-3xl font-bold">My First Blog Post</h1>
+	<p class="mb-6 text-gray-500">
 		<span class="blog-post-date">10/09/2024</span>
-		by <span class="blog-post-author">site-admin</span>
+		by <span class="blog-post-author">zavos.dev</span>
 	</p>
 
-	<div class="blog-post-content"></div>
+	<div class="blog-post-content">
+		Hello there! Welcome to my personal website. I'm excited to share this space with you as I
+		embark on a journey to learn and master Svelte.<br />
+		This website serves as a playground for me to experiment with Svelte, a modern front-end framework
+		known for its reactivity and performance.<br />
+		As I follow tutorials and work through various projects, I'll be documenting my progress and sharing
+		what I learn along the way.
+	</div>
 </article>
 
 <style>
-	.blog-post {
-		margin-bottom: 2rem;
-	}
-
-	.blog-post-title {
-		font-size: 2rem;
-		margin-bottom: 1rem;
-	}
-
-	.blog-post-meta {
-		margin-bottom: 1rem;
-		color: #888;
-	}
-
 	.blog-post-date {
 		margin-right: 1rem;
 	}
