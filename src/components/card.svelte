9802cc30fc18a97a3537c652ed863b44b542d114
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-10 11:09:05 +0300

    Change value of card:hover from 1.1 to 1.05

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <a href={link} class="card-link">
-	<div class="card bg-neutral overflow-hidden rounded-lg shadow-lg">
+	<div class="card overflow-hidden rounded-lg bg-neutral shadow-lg">
 		<img src={imageSrc} alt="card" class="h-48 w-full object-cover" />
 		<div class="p-4">
 			<p>{description}</p>
@@ -19,6 +19,6 @@
 		transition: transform 0.2s ease;
 	}
 	.card:hover {
-		transform: scale(1.1);
+		transform: scale(1.05);
 	}
 </style>
