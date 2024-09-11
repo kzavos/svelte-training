34b8ba8ab9fa34634af5cd1020ce468aae477627
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-11 20:40:00 +0300

    Create the first part of the onmount function tutorial of the 6th article

---
+++ src/routes/projects/project-6/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { paint } from './gradient.js';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const canvas = document.querySelector('canvas');
+		const context = canvas.getContext('2d');
+
+		let frame = requestAnimationFrame(function loop(t) {
+			frame = requestAnimationFrame(loop);
+			paint(context, t);
+		});
+
+		return () => {
+			cancelAnimationFrame(frame);
+		};
+	});
+</script>
+
+<article class="container mx-auto max-w-3xl break-words px-4 py-8">
+	<h1 class="mb-4 text-3xl font-bold">Lifecycles</h1>
+	<p class="mb-6 text-gray-500">
+		<span class="mr-4">11/09/2024</span>
+		by <span class="prose italic">zavos.dev</span>
+	</p>
+
+	<div class="prose text-lg leading-relaxed">
+		Hello there, fellow Svelte enthusiasts! <br />
+		Today, we're exploring <b>lifecycles</b>. Lifecycles are essentially functions that run at
+		specific points in a component's existence, allowing you to control its behavior during
+		initialization, updates, and deconstruction.<br />
+	</div>
+	<br />
+	<div class="prose text-3xl font-bold leading-relaxed">onMount</div>
+	<div class="prose text-lg leading-relaxed">
+		The most common function you will be using to run code at key moments is <b>onMount</b> which runs
+		after the component is first rendered to the DOM.
+	</div>
+	<canvas width="200" height="150"></canvas>
+</article>
+
+<style>
+	/* styling here */
+	canvas {
+		width: 100%;
+		height: 100%;
+		background-color: #666;
+		mask: url(./svelte-logo-mask.svg) 50% 50% no-repeat;
+		mask-size: 60vmin;
+		-webkit-mask: url(./svelte-logo-mask.svg) 50% 50% no-repeat;
+		-webkit-mask-size: 60vmin;
+	}
+</style>
