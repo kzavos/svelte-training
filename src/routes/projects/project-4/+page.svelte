2e5c7cbf453dc17ac96c01000b7cc7c4676c2fc0
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-11 11:52:55 +0300

    Change image of the card and create placeholder text inside of the 4th blogpost

---
+++ src/routes/projects/project-4/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<article class="container mx-auto max-w-3xl break-words px-4 py-8">
+	<h1 class="mb-4 text-3xl font-bold">Events forwarding</h1>
+	<p class="mb-6 text-gray-500">
+		<span class="mr-4">11/09/2024</span>
+		by <span class="prose italic">zavos.dev</span>
+	</p>
+
+	<div class="prose text-lg leading-relaxed">
+		Hey there, fellow Svelte enthusiasts! <br />Today, we're diving into the world of event
+		modifiers powerful tools in Svelte that help us control how events interact with our components.<br
+		/>
+	</div>
+	<div class="container mx-auto max-w-3xl break-words px-4 py-8">
+		<p class="text-large">placeholder text</p>
+	</div>
+
+	<br />
+	<p>You can chain modifiers together,<br /> e.g. <code>on:click|once|capture=""</code>.</p>
+</article>
+
+<style>
+	/* styling here */
+</style>
