d1da595d713e23ec79714f0ae802e37f820715f9
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-13 11:05:42 +0300

    Change date on the 8th project

--- src/routes/projects/project-8/+page.svelte
+++ src/routes/projects/project-8/+page.svelte
@@ -4,7 +4,7 @@
 <article class="container mx-auto max-w-3xl break-words px-4 py-8">
 	<h1 class="mb-4 text-3xl font-bold">Events / Event modifiers</h1>
 	<p class="mb-6 text-gray-500">
-		<span class="mr-4">10/09/2024</span>
+		<span class="mr-4">13/09/2024</span>
 		by <span class="prose italic">zavos.dev</span>
 	</p>
 
