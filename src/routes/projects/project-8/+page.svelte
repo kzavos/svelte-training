0cfcdfcd805067415bed89a6017f02d770bc0f00
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-13 11:37:03 +0300

    Add the non-solved part of the tweening tutorial on 8th project

--- src/routes/projects/project-8/+page.svelte
+++ src/routes/projects/project-8/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { writable } from 'svelte/store';
+	const progress = writable(0);
 </script>
 
 <article class="container mx-auto max-w-3xl break-words px-4 py-8">
@@ -12,6 +14,22 @@
 		Hey there, fellow Svelte enthusiasts! <br />
 		Today, we'll be working on using motion in Svelte to create animations!<br />
 	</div>
+	<br />
+	<h1 class="prose text-3xl font-bold">Tweening</h1>
+	<br />
+	<div class="prose text-lg leading-relaxed">
+		Tweening is a computer graphics technique that involves creating smooth transitions between
+		different states or values of an object over time.
+	</div>
+	<br />
+	<progress class="w-full rounded-full bg-gray-200 dark:bg-gray-700" value={$progress}></progress>
+	<br />
+	<br />
+	<button class="btn" on:click={() => progress.set(0)}>0%</button>
+	<button class="btn" on:click={() => progress.set(0.25)}>25%</button>
+	<button class="btn" on:click={() => progress.set(0.5)}>50%</button>
+	<button class="btn" on:click={() => progress.set(0.75)}>75%</button>
+	<button class="btn" on:click={() => progress.set(1)}>100%</button>
 </article>
 
 <style>
