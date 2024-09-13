8c23b12f6f870987567d8551ff59cded28ba4408
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-13 17:19:35 +0300

    Add the last example from the transitions page

--- src/routes/projects/project-9/+page.svelte
+++ src/routes/projects/project-9/+page.svelte
@@ -5,6 +5,17 @@
 	import { slide } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import { messages } from './loading-messages.js';
+	import { createTodoStore } from './todos.js';
+	import TodoList from './TodoList.svelte';
+
+	const todos = createTodoStore([
+		{ done: false, description: 'write some docs' },
+		{ done: false, description: 'start writing blog post' },
+		{ done: true, description: 'Finish this article' },
+		{ done: false, description: 'mow the lawn' },
+		{ done: false, description: 'feed the turtle' },
+		{ done: false, description: 'fix some bugs' }
+	]);
 
 	let visible = true;
 	let visible2 = true;
@@ -264,8 +275,12 @@
 		<br />
 		Take this pair of todo lists, in which toggling a todo sends it to the opposite list. In the real
 		world, objects don't behave like that — instead of disappearing and reappearing in another place,
-		they move through a series of intermediate positions. Using motion can go a long way towards helping
-		users understand what's happening in your app.
+		they move through a series of intermediate positions.
+		<br />
+		<br />
+		<b
+			>Using motion can go a long way towards helping users understand what's happening in your app.</b
+		>
 		<br />
 		<br />
 		We can achieve this effect using the crossfade function, as seen in transition.js, which creates
@@ -275,8 +290,49 @@
 		the fallback transition is used.
 	</div>
 	<br />
+	<br />
+	<div class="board">
+		<input
+			placeholder="what needs to be done?"
+			on:keydown={(e) => {
+				if (e.key !== 'Enter') return;
+
+				todos.add(e.currentTarget.value);
+				e.currentTarget.value = '';
+			}}
+		/>
+
+		<div class="todo">
+			<h2>todo</h2>
+			<TodoList store={todos} done={false} />
+		</div>
+
+		<div class="done">
+			<h2>done</h2>
+			<TodoList store={todos} done={true} />
+		</div>
+	</div>
 </article>
 
 <style>
 	/* styling here */
+	.board {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-column-gap: 1em;
+		max-width: 36em;
+		margin: 0 auto;
+	}
+
+	.board > input {
+		font-size: 1.4em;
+		grid-column: 1/3;
+		padding: 0.5em;
+		margin: 0 0 1rem 0;
+	}
+
+	h2 {
+		font-size: 2em;
+		font-weight: 200;
+	}
 </style>
