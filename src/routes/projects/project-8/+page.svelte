8b06a892d317bad17d61deb118604c9895d32d7c
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-13 11:38:03 +0300

    Fix a typo on the 8th project

--- src/routes/projects/project-8/+page.svelte
+++ src/routes/projects/project-8/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <article class="container mx-auto max-w-3xl break-words px-4 py-8">
-	<h1 class="mb-4 text-3xl font-bold">Events / Event modifiers</h1>
+	<h1 class="mb-4 text-3xl font-bold">Motion</h1>
 	<p class="mb-6 text-gray-500">
 		<span class="mr-4">13/09/2024</span>
 		by <span class="prose italic">zavos.dev</span>
