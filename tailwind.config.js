85729d3c628a7fc3f33bbc1d885a453bab8ecbe9
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-16 10:44:53 +0000

    Force global dark mode with daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'media',
+	darkMode: 'class',
 	content: [],
 	content: ['./src/**/*.{html,svelte,js,ts}'],
 	theme: {
 	  extend: {},
 	},
 	plugins: [],
-	plugins: [require('daisyui')],
+	plugins: [require('daisyui'),    
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),],
+	daisyui: {
+			themes: ["dark"],
+		  },
    }
