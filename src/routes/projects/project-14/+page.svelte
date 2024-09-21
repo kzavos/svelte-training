ef296e5089e7ee6f12fb755cf5795c2494ff73ac
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-21 12:17:30 +0000

    start working on the special elements part of the tutorial:

---
+++ src/routes/projects/project-14/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<article class="container mx-auto max-w-3xl break-words px-4 py-8">
+	<h1 class="mb-4 text-3xl font-bold">Special elements</h1>
+	<p class="mb-6 text-gray-500">
+		<span class="mr-4">21/09/2024</span>
+		by <span class="prose italic">zavos.dev</span>
+	</p>
+
+	<div class="prose text-lg leading-relaxed">
+		<p>Hello there, fellow Svelte enthusiasts!</p>
+		<br />
+		<p>
+			Svelte provides a variety of built-in elements that we can use.
+		</p>
+	</div>
+	<br />
+	<div class="prose text-3xl font-bold">svelte:self</div>
+	<br />
+
+	<!-- CODE EXAMPLES VERY GOOD TEMPLATE -->
+	<div class="rounded-lg bg-base-200 p-6 shadow-lg">
+		<h2 class="mb-4 text-lg font-semibold">Your HTML Code</h2>
+		<pre class="overflow-auto whitespace-pre-wrap rounded-md bg-gray-900 p-4 text-gray-200">
+		  &lt;div class="example"&gt;
+			&lt;p&gt;This is an example paragraph.&lt;/p&gt;
+		  &lt;/div&gt;
+		</pre>
+	</div>
+</article>
+
+<style>
+	/* styling here */
+</style>
