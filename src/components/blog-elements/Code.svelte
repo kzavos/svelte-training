733ec8875741a364440f15601372e627041bd40b
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-09-23 10:03:53 +0000

    Create reusable code component

---
+++ src/components/blog-elements/Code.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let fileName;
+	export let codeBody;
+</script>
+
+<div class="rounded-lg bg-base-200 p-6 shadow-lg">
+	<h2 class="mb-4 text-lg font-semibold">{fileName}</h2>
+	<pre class="overflow-auto whitespace-pre-wrap rounded-md bg-gray-900 p-4 text-gray-200">
+        {codeBody}
+    </pre>
+</div>
