6fbf024fb1458c32d0a057aa99a8ce522cb9f985
Kzavos <115787279+kzavos@users.noreply.github.com>
2024-10-07 13:11:48 +0300

    Fix strange looking article titles but mess up the cat example

--- src/routes/projects/project-14/+page.svelte
+++ src/routes/projects/project-14/+page.svelte
@@ -122,26 +122,26 @@
 
 	// svelte:fragment
 	import Board from './Board.svelte';
-	
-	const lines = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-];
 
-function getWinningLine(squares) {
-	for (const line of lines) {
-		const [a, b, c] = line;
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return line;
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+
+	function getWinningLine(squares) {
+		for (const line of lines) {
+			const [a, b, c] = line;
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return line;
+			}
 		}
 	}
-}
 
 	let squares = Array(9).fill('');
 	let next = 'x';
@@ -332,35 +332,36 @@ function getWinningLine(squares) {
 	</div>
 	<br />
 
-<div class="container">
-	<Board size={3}>
-		<div slot="game">
-			{#each squares as square, i}
+	<div class="container">
+		<Board size={3}>
+			<div slot="game">
+				{#each squares as square, i}
+					<button
+						class="square"
+						class:winning={winningLine?.includes(i)}
+						disabled={square}
+						on:click={() => {
+							squares[i] = next;
+							next = next === 'x' ? 'o' : 'x';
+						}}
+					>
+						{square}
+					</button>
+				{/each}
+			</div>
+
+			<div slot="controls">
 				<button
-					class="square"
-					class:winning={winningLine?.includes(i)}
-					disabled={square}
 					on:click={() => {
-						squares[i] = next;
-						next = next === 'x' ? 'o' : 'x';
+						squares = Array(9).fill('');
+						next = 'x';
 					}}
 				>
-					{square}
+					Reset
 				</button>
-			{/each}
-		</div>
-
-		<div slot="controls">
-			<button on:click={() => {
-				squares = Array(9).fill('');
-				next = 'x';
-			}}>
-				Reset
-			</button>
-		</div>
-	</Board>
-</div>
-
+			</div>
+		</Board>
+	</div>
 </article>
 
 <svelte:window bind:scrollY={y} on:keydown={handleKeyDown} />
@@ -419,13 +420,14 @@ function getWinningLine(squares) {
 		display: flex;
 		flex-direction: column;
 		gap: 1em;
-		align-items: center;
+		/* align-items: center; */
 		justify-content: center;
 		height: 100%;
 		margin: 0 auto;
 	}
 
-	.square, .square:disabled {
+	.square,
+	.square:disabled {
 		background: white;
 		border-radius: 0;
 		color: #222;
@@ -441,4 +443,6 @@ function getWinningLine(squares) {
 	.container:has(.winning) .square:not(.winning) {
 		color: #ccc;
 	}
+
+
 </style>
